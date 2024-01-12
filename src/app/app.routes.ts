582f681469b3a4c26c31505cd30f2013d27219eb
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-12 17:56:27 +0100

    Create login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './domains/login/ui/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        title: 'Login | QZed',
+        component: LoginPageComponent
+    }
+];
