49fd60e01d3b44cc49d365fc002845ba29672368
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-13 14:41:51 +0100

    Display name & photo on header component

--- src/app/domains/study/feature/header/header.component.ts
+++ src/app/domains/study/feature/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  @Input({required: true}) uid: string = '';
+  private firestore: Firestore = inject(Firestore);
+  user: { displayName: string, photoURL: string } = { displayName: '', photoURL: '' };
 
+  ngOnInit() {
+    this.getUserDetails();
+  }
+
+  async getUserDetails(): Promise<void> {
+    const userDoc = await getDoc(doc(this.firestore, `users/${this.uid}`));
+    const userData = userDoc.data()?.['user'];
+    if (userData) {
+      this.user['displayName'] = userData['displayName'];
+      this.user['photoURL'] = userData['photoURL'];
+    }
+  }
 }
