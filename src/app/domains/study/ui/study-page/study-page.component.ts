9eda9b38ff7fca9d351aad52c2b01f4c38016794
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-13 14:33:34 +0100

    Add header component to study-page component

--- src/app/domains/study/ui/study-page/study-page.component.ts
+++ src/app/domains/study/ui/study-page/study-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { HeaderComponent } from '../../feature/header/header.component';
 
 @Component({
   selector: 'study-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './study-page.component.html',
   styleUrl: './study-page.component.scss'
 })
