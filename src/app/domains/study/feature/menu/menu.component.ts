4a857108370115f9b35908f32d783bf4f95f8e99
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-15 15:18:40 +0100

    Partially set up UI of menu componennt

--- src/app/domains/study/feature/menu/menu.component.ts
+++ src/app/domains/study/feature/menu/menu.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+  isCollapsed = false;
 
+  menuItems: { [key: string]: { route: string; selected: boolean } } = {
+    dashboard: { route:'/dashboard', selected: false },
+    question: { route: '/question-bank', selected: true },
+    download: { route: '/download', selected: false }
+  };
+
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  switchRoute(route: string) {
+    for (let key in this.menuItems) {
+      this.menuItems[key].selected = false;
+    }
+    this.menuItems[route].selected = true;
+  }
 }
