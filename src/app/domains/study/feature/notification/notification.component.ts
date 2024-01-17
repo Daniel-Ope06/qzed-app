3bd1ed8691d8c5498faf95c67bb240556fd03210
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-17 11:21:09 +0100

    Add title and content to notification

--- src/app/domains/study/feature/notification/notification.component.ts
+++ src/app/domains/study/feature/notification/notification.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-notification',
+  selector: 'notification',
   standalone: true,
   imports: [],
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.scss'
 })
 export class NotificationComponent {
-
+  @Input({required: true}) title: string = '';
+  @Input({required: true}) content: string = '';
+  @Input({required: true}) date: string = '';
+  @Input({required: true}) uid: string = '';
 }
