d499e67bf92acab76260b755f85df9fb618491b7
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-18 00:47:46 +0100

    Set up notification service to getAll() notifications

--- src/app/domains/study/data-access/notification.service.ts
+++ src/app/domains/study/data-access/notification.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import { Notification } from './notification.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
+  firestore: Firestore = inject(Firestore);
 
-  constructor() { }
+  async getAll(): Promise<Notification[]> {
+    let notifications: Notification[] = [];
+    const querySnapshot = await getDocs(collection(this.firestore, 'notifications'));
+    querySnapshot.forEach((doc) => {
+      notifications.push(doc.data() as Notification);
+    })
+    return notifications;
+  }
 }
