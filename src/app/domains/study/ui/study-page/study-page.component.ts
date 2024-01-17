ee02e25a0ced66a36cbba29c4c8d42738e7202e6
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-17 11:22:04 +0100

    Add dashboard to study component

--- src/app/domains/study/ui/study-page/study-page.component.ts
+++ src/app/domains/study/ui/study-page/study-page.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { DashboardComponent } from "../../feature/dashboard/dashboard.component";
 import { HeaderComponent } from '../../feature/header/header.component';
 import { MenuComponent } from '../../feature/menu/menu.component';
 
 @Component({
-  selector: 'study-page',
-  standalone: true,
-  imports: [HeaderComponent, MenuComponent],
-  templateUrl: './study-page.component.html',
-  styleUrl: './study-page.component.scss'
+    selector: 'study-page',
+    standalone: true,
+    templateUrl: './study-page.component.html',
+    styleUrl: './study-page.component.scss',
+    imports: [DashboardComponent, HeaderComponent, MenuComponent]
 })
 export class StudyPageComponent {
   @Input() uid: string = '';
