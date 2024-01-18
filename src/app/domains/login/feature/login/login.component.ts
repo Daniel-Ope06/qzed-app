ef9e4edc47538cbe197328dc68819b3f458e2e00
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-18 18:44:58 +0100

    Set up Guest Login in login component

--- src/app/domains/login/feature/login/login.component.ts
+++ src/app/domains/login/feature/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { Auth, GoogleAuthProvider, User, onAuthStateChanged, signInWithRedirect } from '@angular/fire/auth';
+import { Auth, GoogleAuthProvider, User, onAuthStateChanged, signInAnonymously, signInWithRedirect } from '@angular/fire/auth';
 import { Firestore, doc, setDoc } from '@angular/fire/firestore';
 import { LoadingSpinnerComponent } from '../../../shared/ui/loading-spinner/loading-spinner.component';
 
@@ -16,7 +16,12 @@ export class LoginComponent implements OnInit {
   private firestore: Firestore = inject(Firestore);
   private googleProvider: GoogleAuthProvider = new GoogleAuthProvider();
   private router = inject(Router);
- isLoading: boolean = false;
+  private anonymousUser = {
+    email: 'guest@email.com',
+    photoURL: 'https://drive.google.com/thumbnail?id=1IzYvnObDukiNA2Bz0I2YnzRkU5OvVSx3',
+    displayName: 'Guest User',
+  };
+  isLoading: boolean = false;
 
   ngOnInit() {
     this.isLoading = true;
@@ -33,16 +38,20 @@ export class LoginComponent implements OnInit {
   private updateUserData(result: User) {
     const userRef = doc(this.firestore, `users/${result.uid}`);
     const user = {
-      uid: result.uid!,
-      email: result.email!,
-      photo_url: result.photoURL!,
-      display_name: result.displayName!,
+      uid: result.uid,
+      email: result.email ?? this.anonymousUser.email,
+      photo_url: result.photoURL ?? this.anonymousUser.photoURL,
+      display_name: result.displayName ?? this.anonymousUser.displayName,
     };
     return setDoc(userRef, { user }, { merge: true });
   }
 
   loginDemo() {
-    this.router.navigate([`study/demo`])
+    this.isLoading = true;
+    signInAnonymously(this.auth).then((result) => {
+      this.updateUserData(result.user);
+      this.router.navigate([`study/${result.user.uid}`]);
+    });
   }
 
   loginGoogle() {
