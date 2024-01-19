b3eceff90c5febbd4182460591860a8cacc52e0a
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-19 23:15:17 +0100

    Add app-list to question-bank

--- src/app/domains/study/feature/question-bank/question-bank.component.ts
+++ src/app/domains/study/feature/question-bank/question-bank.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'question-bank',
   standalone: true,
-  imports: [],
+  imports: [ListComponent],
   templateUrl: './question-bank.component.html',
   styleUrl: './question-bank.component.scss'
 })
 export class QuestionBankComponent {
-
+  isGrid: boolean = false;
 }
