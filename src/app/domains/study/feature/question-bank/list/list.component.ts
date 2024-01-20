0e39b8ffbeb56ac7f37e4500a2d88fc3594f09c8
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-21 00:21:29 +0100

    Set up list component

--- src/app/domains/study/feature/question-bank/list/list.component.ts
+++ src/app/domains/study/feature/question-bank/list/list.component.ts
@@ -1,15 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoadingSpinnerComponent } from '../../../../shared/ui/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoadingSpinnerComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
-  @Input({required: true}) isGrid: boolean = true;
-  @Input() items: string[] = Array(13).fill('EU');
-  //{required: true}
+  @Input({required: true}) items: {long:string, short:string, id:string}[] = [];
+  @Output() selectItemEvent = new EventEmitter<string>();
+
+  isGrid: boolean = false;
+
+  returnId(id: string) {
+    this.selectItemEvent.emit(id);
+  }
 }
