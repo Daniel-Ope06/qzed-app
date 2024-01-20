202c9223ada8a724ef365bde01f45136bb4231db
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-21 00:19:57 +0100

    Create getSchools() & getCourses()

--- src/app/domains/study/data-access/question.service.ts
+++ src/app/domains/study/data-access/question.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { DocumentData, Firestore, collection, getDocs, orderBy, where, query } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
+  firestore: Firestore = inject(Firestore);
 
-  constructor() { }
+  async getSchools() {
+    let schools: DocumentData[] = [];
+    const schoolsRef = collection(this.firestore, 'schools');
+    const q = query(schoolsRef, orderBy('full_name'));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      schools.push(doc.data());
+    });
+    return schools;
+  }
+
+  async getCourses(schoolId: string) {
+    const courseRef = collection(this.firestore, 'schools', schoolId, 'courses');
+    const querySnapshot = await getDocs(courseRef);
+    const courses = querySnapshot.docs.map((doc) => doc.data());
+    return courses;
+  }
 }
