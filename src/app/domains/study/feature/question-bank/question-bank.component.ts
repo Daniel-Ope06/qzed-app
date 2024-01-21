3561d2b26b0bfbfdf9b4e4e3e159fa8d19ca5d38
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-21 21:51:55 +0100

    Respond to course-list event in question-bank

--- src/app/domains/study/feature/question-bank/question-bank.component.ts
+++ src/app/domains/study/feature/question-bank/question-bank.component.ts
@@ -1,14 +1,13 @@
-import { Component, Input, inject } from '@angular/core';
-import { ListComponent } from './list/list.component';
-import { QuestionService } from '../../data-access/question.service';
+import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { SchoolListComponent } from './school-list/school-list.component';
+import { CourseListComponent } from './course-list/course-list.component';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'question-bank',
   standalone: true,
-  imports: [ListComponent, RouterModule],
+  imports: [RouterModule],
   templateUrl: './question-bank.component.html',
   styleUrl: './question-bank.component.scss'
 })
@@ -21,6 +20,11 @@ export class QuestionBankComponent {
       const schoolList = componentRef as SchoolListComponent;
       schoolList.selectSchoolEvent.subscribe((id) => {
         this.routeToCourseList(id);
+      });
+    } else if (componentRef instanceof CourseListComponent) {
+      const courseList = componentRef as CourseListComponent;
+      this.subscription = courseList.selectCourseEvent.subscribe((id) => {
+        this.routeToYearList(id);
       });
     }
   }
@@ -38,62 +42,10 @@ export class QuestionBankComponent {
     this.router.navigate([`study/${uid}/question-bank/schools/${schoolId}`]);
   }
 
-
-  @Input() section: string = 'schools';
-  private questionService: QuestionService = inject(QuestionService);
-
-  isGrid: boolean = false;
-  items: {name:string, id:string}[] = [];
-  schools: any[] = [];
-  courses: any[] = [];
-
-
-  
-
-
-
-  async receiveId(id: string) {
-    console.log(id);
-    await this.getCourses(id);
-    this.setItemsToCourses();
-    console.log(this.courses);
-  }
-
-  async getSchools() {
-    await this.questionService.getSchools().then((schools) => {
-      this.schools = schools;
-    });
-  }
-
-  async getCourses(schoolId: string) {
-    await this.questionService.getCourses(schoolId).then((courses) => {
-      this.courses = courses;
-    });
-  }
-
-  setItemsToSchools() {
-    this.items = [];
-    if (this.isGrid) {
-      this.schools.forEach((school) => {
-        this.items.push({name: school.short_name, id: school.id});
-      });
-    } else {
-      this.schools.forEach((school) => {
-        this.items.push({name: school.full_name, id: school.id});
-      });
-    }
-  }
-
-  setItemsToCourses() {
-    this.items = [];
-    if (this.isGrid) {
-      this.courses.forEach((course) => {
-        this.items.push({name: course.code, id: course.id});
-      });
-    } else {
-      this.courses.forEach((course) => {
-        this.items.push({name: course.name, id: course.id});
-      });
-    }
+  routeToYearList(courseId: string) {
+    const currentRoute = this.router.url.substring(1);
+    const segments = currentRoute.split('/');
+    const uid = segments[1];
+    this.router.navigate([`study/${uid}/question-bank/courses/${courseId}`]);
   }
 }
