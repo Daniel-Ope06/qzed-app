5182fe3dc101d88046d82e3409ef35c4317d0f98
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-21 21:51:26 +0100

    Set up course-list component

--- src/app/domains/study/feature/question-bank/course-list/course-list.component.ts
+++ src/app/domains/study/feature/question-bank/course-list/course-list.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { QuestionService } from '../../../data-access/question.service';
+import { ListComponent } from '../list/list.component';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [],
+  imports: [ListComponent],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.scss'
 })
 export class CourseListComponent {
+  @Input({required: true}) schoolId: string = '';
+  @Output() selectCourseEvent = new EventEmitter<string>();
+  private questionService = inject(QuestionService);
+  items: {long:string, short:string, id:string}[] = [];
+  heading: string = 'Select a course';
 
+  async ngOnInit() {
+    await this.questionService.getCourses(this.schoolId).then((courses) => {
+      for (let course of courses) {
+        this.items.push({long: course['name'], short: course['code'], id: course['id']});
+      }
+    });
+  }
+
+  returnCourseId(id: string) {
+    this.selectCourseEvent.emit(id);
+  }
 }
