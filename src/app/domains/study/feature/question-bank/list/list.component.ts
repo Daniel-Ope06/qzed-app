789d331311ce8257447b2400ff72c3f0b3109d25
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-21 01:12:36 +0100

    Set up grid and flex lists UI for list component

--- src/app/domains/study/feature/question-bank/list/list.component.ts
+++ src/app/domains/study/feature/question-bank/list/list.component.ts
@@ -11,9 +11,14 @@ import { LoadingSpinnerComponent } from '../../../../shared/ui/loading-spinner/l
 })
 export class ListComponent {
   @Input({required: true}) items: {long:string, short:string, id:string}[] = [];
+  @Input({required: true}) heading: string = '';
   @Output() selectItemEvent = new EventEmitter<string>();
 
   isGrid: boolean = false;
+
+  toggleGrid() {
+    this.isGrid = !this.isGrid;
+  }
 
   returnId(id: string) {
     this.selectItemEvent.emit(id);
