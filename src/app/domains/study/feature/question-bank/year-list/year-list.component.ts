b7488fb797e6ec2c888d11fc4cbd3c47474ab374
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-01-22 01:14:34 +0100

    Set up year-list component

--- src/app/domains/study/feature/question-bank/year-list/year-list.component.ts
+++ src/app/domains/study/feature/question-bank/year-list/year-list.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { QuestionService } from '../../../data-access/question.service';
+import { ListComponent } from '../list/list.component';
 
 @Component({
   selector: 'app-year-list',
   standalone: true,
-  imports: [],
+  imports: [ListComponent],
   templateUrl: './year-list.component.html',
   styleUrl: './year-list.component.scss'
 })
 export class YearListComponent {
+  @Input({required: true}) courseId: string = '';
+  @Input({required: true}) schoolId: string = '';
+  @Output() selectYearEvent = new EventEmitter<string>();
 
+  private questionService = inject(QuestionService);
+  items: {long:string, short:string, id:string}[] = [];
+  heading: string = 'Select a year';;
+
+  async ngOnInit() {
+    await this.questionService.getCourse(this.schoolId, this.courseId).then((course) => {
+      for (const [key, value] of Object.entries(course!['years'])) {
+        this.items.push({long: key, short: key, id: value as string});
+      }
+    });
+  }
+
+  returnYearURL(url: string) {
+    this.selectYearEvent.emit(url);
+  }
 }
