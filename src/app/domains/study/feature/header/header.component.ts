0a082528826c33b326dfeec55d3c229119a3bfe1
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-06 21:49:15 +0100

    Use cat.jpg if 403 error occurs for profile photo

--- src/app/domains/study/feature/header/header.component.ts
+++ src/app/domains/study/feature/header/header.component.ts
@@ -27,4 +27,8 @@ export class HeaderComponent implements OnInit {
       this.user['photoURL'] = userData['photo_url'];
     }
   }
+
+  replaceImage() {
+    this.user['photoURL'] = '../../../../../assets/study/cat.jpg';
+  }
 }
