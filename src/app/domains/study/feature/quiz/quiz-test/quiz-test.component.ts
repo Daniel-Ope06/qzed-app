69ed064bd41874e76c69703dc913d976666bfb47
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-07 17:01:15 +0100

    Create quiz-test component

---
+++ src/app/domains/study/feature/quiz/quiz-test/quiz-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-test.component.html',
+  styleUrl: './quiz-test.component.scss'
+})
+export class QuizTestComponent {
+
+}
