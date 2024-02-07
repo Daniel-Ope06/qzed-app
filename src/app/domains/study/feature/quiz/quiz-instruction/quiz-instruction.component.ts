df07082856ed5dbd42ed4801695f159a0c66efc7
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-07 18:04:25 +0100

    Create quiz-instruction component

---
+++ src/app/domains/study/feature/quiz/quiz-instruction/quiz-instruction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quiz-instruction',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-instruction.component.html',
+  styleUrl: './quiz-instruction.component.scss'
+})
+export class QuizInstructionComponent {
+
+}
