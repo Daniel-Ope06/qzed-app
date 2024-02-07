5de2f14542641393f9282e46ff4138d8d8c676c7
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-07 22:19:24 +0100

    Set up routing to quiz-list & quiz-test

--- src/app/domains/study/feature/quiz/quiz.component.ts
+++ src/app/domains/study/feature/quiz/quiz.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { SchoolListComponent } from '../../../shared/ui/school-list/school-list.component';
+import { QuizListComponent } from './quiz-list/quiz-list.component';
 
 @Component({
   selector: 'app-quiz',
@@ -20,6 +21,11 @@ export class QuizComponent {
       schoolList.selectSchoolEvent.subscribe((schoolId) => {
         this.routeToQuizList(schoolId);
       });
+    } else if (componentRef instanceof QuizListComponent) {
+      const quizList = componentRef as QuizListComponent;
+      this.subscription = quizList.selectQuizEvent.subscribe((courseId) => {
+        this.routeToQuizTest(courseId);
+      });
     }
   }
 
@@ -34,5 +40,22 @@ export class QuizComponent {
     const segments = currentRoute.split('/');
     const uid = segments[1];
     this.router.navigate([`study/${uid}/quiz/${schoolId}`]);
+  }
+
+  routeToQuizTest(courseId: string) {
+    const currentRoute = this.router.url.substring(1);
+    const segments = currentRoute.split('/');
+    const uid = segments[1];
+    const schoolId = segments[3];
+    this.router.navigate([`study/${uid}/quiz/${schoolId}/${courseId}`]);
+  }
+
+  isQuizTestComponent() {
+    const currentRoute = this.router.url.substring(1);
+    const segments = currentRoute.split('/');
+    if (segments.length === 5) {
+      return true;
+    }
+    return false;
   }
 }
