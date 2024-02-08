1dad5f9b236be0aa2f84909ebc129548b8c9a231
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-08 17:24:56 +0100

    Set up quiz-instructions to not rely on  @Input

--- src/app/domains/study/feature/quiz/quiz-instruction/quiz-instruction.component.ts
+++ src/app/domains/study/feature/quiz/quiz-instruction/quiz-instruction.component.ts
@@ -1,20 +1,37 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { QuestionService } from '../../../data-access/question.service';
+import { LoadingSpinnerComponent } from '../../../../shared/ui/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'quiz-instruction',
   standalone: true,
-  imports: [],
+  imports: [LoadingSpinnerComponent],
   templateUrl: './quiz-instruction.component.html',
   styleUrl: './quiz-instruction.component.scss'
 })
 export class QuizInstructionComponent {
-  @Input({required: true}) courseCode: string = '';
-  @Input({required: true}) timeInMins: number = 0;
-  @Input({required: true}) numOfQuestions: number = 0;
-  @Input({required: true}) pointsPerQuestion: number = 0;
-  @Output() startQuizEvent = new EventEmitter<boolean>();
+  @Output() startQuizEvent = new EventEmitter<void>();
 
-  startQuiz() {
-    this.startQuizEvent.emit(true);
+  private router = inject(Router);
+  private questionService = inject(QuestionService);
+
+  courseCode: string = '';
+  timeInMins: number = 0;
+  numOfQuestions: number = 0;
+  pointsPerQuestion: number = 0;
+
+  async ngOnInit() {
+    const currentRoute = this.router.url.substring(1);
+    const segments = currentRoute.split('/');
+    const schoolId = segments[3];
+    const courseId = segments[4];
+
+    await this.questionService.getCourse(schoolId, courseId).then((course) => {
+      this.courseCode = course!['code'];
+      this.timeInMins = course!['quiz']['time_in_mins'];
+      this.numOfQuestions = course!['quiz']['num_of_questions'];
+      this.pointsPerQuestion = 100 / this.numOfQuestions;
+    });
   }
 }
