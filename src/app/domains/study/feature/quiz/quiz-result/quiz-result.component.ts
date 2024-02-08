04a7b93b501b82b614649157347c758dfd49b761
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-08 18:13:19 +0100

    Create quiz-result component

---
+++ src/app/domains/study/feature/quiz/quiz-result/quiz-result.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-result',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-result.component.html',
+  styleUrl: './quiz-result.component.scss'
+})
+export class QuizResultComponent {
+
+}
