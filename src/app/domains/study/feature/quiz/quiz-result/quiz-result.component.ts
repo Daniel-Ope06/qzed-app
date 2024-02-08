f695d70cf1f838b6aa7b2a0f8027372d70d23107
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-08 19:26:21 +0100

    Set up quiz-result component

--- src/app/domains/study/feature/quiz/quiz-result/quiz-result.component.ts
+++ src/app/domains/study/feature/quiz/quiz-result/quiz-result.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { QuizResultService } from './quiz-result.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quiz-result',
@@ -8,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './quiz-result.component.scss'
 })
 export class QuizResultComponent {
+  private quizResultService = inject(QuizResultService);
+  private router = inject(Router);
+  result: number = 0;
 
+  ngOnInit() {
+    this.quizResultService.result.subscribe((result) => {
+      this.result = result;
+    });
+  }
+
+  routeToQuiz() {
+    const currentRoute = this.router.url.substring(1);
+    const segments = currentRoute.split('/');
+    const uid = segments[1];
+    const schoolId = segments[3];
+    const courseId = segments[4];
+    this.router.navigate([`study/${uid}/quiz/${schoolId}/${courseId}`]);
+  }
 }
