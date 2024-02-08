7643d72ac6f543714a338db8a6b0e05d7773ed01
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-08 22:01:52 +0100

    Restructure quiz-test to rely on routing

--- src/app/domains/study/feature/quiz/quiz-test/quiz-test.component.ts
+++ src/app/domains/study/feature/quiz/quiz-test/quiz-test.component.ts
@@ -1,41 +1,59 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { QuestionService } from '../../../data-access/question.service';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { QuizInstructionComponent } from '../quiz-instruction/quiz-instruction.component';
-import { Router } from '@angular/router';
+import { QuizQuestionsComponent } from '../quiz-questions/quiz-questions.component';
+import { QuizResultService } from '../quiz-result/quiz-result.service';
+
 
 @Component({
   selector: 'app-quiz-test',
   standalone: true,
-  imports: [QuizInstructionComponent],
+  imports: [QuizInstructionComponent, QuizQuestionsComponent, RouterModule],
   templateUrl: './quiz-test.component.html',
   styleUrl: './quiz-test.component.scss'
 })
-export class QuizTestComponent implements OnInit {
-  courseCode: string = '';
-  timeInMins: number = 0;
-  numOfQuestions: number = 0;
-  quizPool: { question: string, answer: string, options: string[] }[] = [];
-
+export class QuizTestComponent {
+  private quizResultService = inject(QuizResultService);
   private router = inject(Router);
-  private questionService = inject(QuestionService);
+  subscription!: Subscription;
 
-  async ngOnInit() {
+  subscribeToEmitter(componentRef: Component) {
+    if (componentRef instanceof QuizInstructionComponent) {
+      const quizInstruction = componentRef as QuizInstructionComponent;
+      quizInstruction.startQuizEvent.subscribe(() => {
+        this.routeToQuizQuestions();
+      });
+    } else if (componentRef instanceof QuizQuestionsComponent) {
+      const quizQuestions = componentRef as QuizQuestionsComponent;
+      this.subscription = quizQuestions.submitQuizEvent.subscribe((percentage) => {
+        this.quizResultService.setResult(percentage);
+        this.routeToQuizResult();
+      });
+    }
+  }
+
+  unsubscribeFromEmitter() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  routeToQuizQuestions() {
     const currentRoute = this.router.url.substring(1);
     const segments = currentRoute.split('/');
+    const uid = segments[1];
     const schoolId = segments[3];
     const courseId = segments[4];
+    this.router.navigate([`study/${uid}/quiz/${schoolId}/${courseId}/questions`]);
+  }
 
-    await this.questionService.getCourse(schoolId, courseId).then((course) => {
-      this.courseCode = course!['code'];
-      this.timeInMins = course!['quiz']['time_in_mins'];
-      this.numOfQuestions = course!['quiz']['num_of_questions'];
-      for (let quiz of course!['quiz']['pool']) {
-        this.quizPool.push({ question: quiz['question'], answer: quiz['answer'], options: quiz['options'] });
-      }
-      console.log(this.courseCode);
-      console.log(this.timeInMins);
-      console.log(this.numOfQuestions);
-      console.log(this.quizPool);
-    });
+  routeToQuizResult() {
+    const currentRoute = this.router.url.substring(1);
+    const segments = currentRoute.split('/');
+    const uid = segments[1];
+    const schoolId = segments[3];
+    const courseId = segments[4];
+    this.router.navigate([`study/${uid}/quiz/${schoolId}/${courseId}/result`]);
   }
 }
