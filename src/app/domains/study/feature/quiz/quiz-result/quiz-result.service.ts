16561c22781e81be4e301f3b71f1508847dd493f
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-08 19:26:05 +0100

    Set up quiz-result service

--- src/app/domains/study/feature/quiz/quiz-result/quiz-result.service.ts
+++ src/app/domains/study/feature/quiz/quiz-result/quiz-result.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizResultService {
+  private resultSource = new BehaviorSubject<number>(-1);
+  result = this.resultSource.asObservable();
 
-  constructor() { }
+  constructor() {
+    const result = localStorage.getItem('quizResult');
+    if (result) {
+      this.resultSource.next(parseInt(result));
+    }
+  }
+
+  setResult(result: number) {
+    this.resultSource.next(result);
+    this.storeInLocalStorage(result);
+  }
+
+  storeInLocalStorage(result: number) {
+    localStorage.setItem('quizResult', result.toString());
+  }
 }
