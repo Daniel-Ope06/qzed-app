4220f8c1391b70ee20d9e486fe03072124268456
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-08 18:11:18 +0100

    Set up quiz-questions component

--- src/app/domains/study/feature/quiz/quiz-questions/quiz-questions.component.ts
+++ src/app/domains/study/feature/quiz/quiz-questions/quiz-questions.component.ts
@@ -1,12 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, inject } from '@angular/core';
+import { QuestionService } from '../../../data-access/question.service';
+import { Router } from '@angular/router';
+import { LoadingSpinnerComponent } from "../../../../shared/ui/loading-spinner/loading-spinner.component";
 
 @Component({
-  selector: 'app-quiz-questions',
-  standalone: true,
-  imports: [],
-  templateUrl: './quiz-questions.component.html',
-  styleUrl: './quiz-questions.component.scss'
+    selector: 'quiz-questions',
+    standalone: true,
+    templateUrl: './quiz-questions.component.html',
+    styleUrl: './quiz-questions.component.scss',
+    imports: [LoadingSpinnerComponent]
 })
-export class QuizQuestionsComponent {
+export class QuizQuestionsComponent implements OnInit {
+  @Output() submitQuizEvent = new EventEmitter<number>();
 
+  private router = inject(Router);
+  private questionService = inject(QuestionService);
+
+  courseCode: string = '';
+  timeInMins: number = 0;
+  numOfQuestions: number = 0;
+  quizPool: { question: string, answer: string, options: string[] }[] = [];
+
+  letters: string[] = ['A', 'B', 'C', 'D'];
+
+  pool: {
+    question: string,
+    answer: string,
+    options: { value: string, isSelected: boolean }[]
+  }[] = [];
+
+  canSubmitQuiz: boolean = false;
+
+
+  async ngOnInit() {
+    const currentRoute = this.router.url.substring(1);
+    const segments = currentRoute.split('/');
+    const schoolId = segments[3];
+    const courseId = segments[4];
+
+    await this.questionService.getCourse(schoolId, courseId).then((course) => {
+      this.courseCode = course!['code'];
+      this.timeInMins = course!['quiz']['time_in_mins'];
+      this.numOfQuestions = course!['quiz']['num_of_questions'];
+      for (let quiz of course!['quiz']['pool']) {
+        this.quizPool.push({ question: quiz['question'], answer: quiz['answer'], options: quiz['options'] });
+      }
+      this.initializePool();
+    });
+  }
+
+
+  initializePool() {
+    for (let quiz of this.quizPool) {
+      let options: { value: string, isSelected: boolean }[] = [];
+      for (let option of quiz['options']) {
+        options.push({ value: option, isSelected: false });
+      }
+      this.pool.push({ question: quiz['question'], answer: quiz['answer'], options: options });
+    }
+  }
+
+
+  selectOption(questionIndex: number, optionIndex: number) {
+    for (let option of this.pool[questionIndex]['options']) {
+      option['isSelected'] = false;
+    }
+    this.pool[questionIndex]['options'][optionIndex]['isSelected'] = true;
+    this.canSubmitQuiz = this.canSubmit();
+  }
+
+  canSubmit(): boolean {
+    for (let question of this.pool) {
+      if (question['options'].find(option => option['isSelected'] === true) === undefined) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  submitQuiz() {
+    let score = 0;
+    for (let question of this.pool) {
+      if (question['options'].find(option => option['isSelected'] === true)!.value === question['answer']) {
+        score++;
+      }
+    }
+    let percentage = (score / this.numOfQuestions) * 100;
+    this.submitQuizEvent.emit(percentage);
+  }
 }
