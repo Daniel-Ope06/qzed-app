2b34f26a935e09d6cc4c264eb0503ec4de7f9676
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-08 13:03:56 +0100

    Set up startQuizEvent

--- src/app/domains/study/feature/quiz/quiz-instruction/quiz-instruction.component.ts
+++ src/app/domains/study/feature/quiz/quiz-instruction/quiz-instruction.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'quiz-instruction',
@@ -12,4 +12,9 @@ export class QuizInstructionComponent {
   @Input({required: true}) timeInMins: number = 0;
   @Input({required: true}) numOfQuestions: number = 0;
   @Input({required: true}) pointsPerQuestion: number = 0;
+  @Output() startQuizEvent = new EventEmitter<boolean>();
+
+  startQuiz() {
+    this.startQuizEvent.emit(true);
+  }
 }
