10e2a207897451ef9836a02f66f9863cb7323412
Daniel-Ope06 <97707320+Daniel-Ope06@users.noreply.github.com>
2024-02-17 11:55:13 +0100

    Remove reliance on uid in header component

--- src/app/domains/study/feature/header/header.component.ts
+++ src/app/domains/study/feature/header/header.component.ts
@@ -10,7 +10,6 @@ import { Router } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
-  @Input({required: true}) uid: string = '';
   @Input({required: true}) tabTitle: string = '';
   private auth: Auth = inject(Auth);
   private router = inject(Router);
@@ -22,20 +21,11 @@ export class HeaderComponent implements OnInit {
         this.user['displayName'] = userData['displayName']?? 'Guest User';
         this.user['email'] = userData['email']?? 'guest@email.com';
         this.user['photoURL'] = userData['photoURL']?? 'https://drive.google.com/thumbnail?id=1IzYvnObDukiNA2Bz0I2YnzRkU5OvVSx3';
-        this.correctUserID(userData['uid']);
       }
     });
   }
 
   replaceImage() {
     this.user['photoURL'] = '../../../../../assets/study/cat.jpg';
-  }
-
-  correctUserID(uid: string) {
-    if (this.uid === uid) { return; }
-    const currentRoute: string = this.router.url.substring(1);
-    const segments: string[] = currentRoute.split('/');
-    segments[1] = uid;
-    this.router.navigate(segments);
   }
 }
