96a27629e1e4d94a6751c023d941dc44af6dec8e
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-06-23 13:25:06 +0100

    Import `hero` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './ui/navbar/navbar.component';
+import { HeroComponent } from './ui/hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
