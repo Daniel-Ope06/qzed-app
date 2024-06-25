525f2bb6c0914ce6567cb1341efde35094feedc3
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-06-25 15:50:45 +0100

    Create `benefit-list` component

---
+++ src/app/domains/home/ui/benefit-list/benefit-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'benefit-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './benefit-list.component.html',
+  styleUrl: './benefit-list.component.scss',
+})
+export class BenefitListComponent {}
