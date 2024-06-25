bbb7e54c4292ed627383efd94f7c388b2c03e8b1
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-06-25 10:00:44 +0100

    Create `user-count` component

---
+++ src/app/domains/home/feature/user-count/user-count.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-count',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-count.component.html',
+  styleUrl: './user-count.component.scss',
+})
+export class UserCountComponent {}
