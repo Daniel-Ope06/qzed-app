49ea2f367ffbd8391a408100864df9f7401ad127
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-06-25 17:12:33 +0100

    Import `benefit-list` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './ui/navbar/navbar.component';
 import { HeroComponent } from './ui/hero/hero.component';
 import { FeatureListComponent } from './ui/feature-list/feature-list.component';
+import { BenefitListComponent } from './ui/benefit-list/benefit-list.component';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent, FeatureListComponent],
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    FeatureListComponent,
+    BenefitListComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
