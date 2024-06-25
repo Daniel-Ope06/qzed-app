e0ebd69ea9d2bb33a92780a5475d0d4bd2b1cba6
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-06-25 13:13:12 +0100

    Import `feature-list` into `home`

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './ui/navbar/navbar.component';
 import { HeroComponent } from './ui/hero/hero.component';
+import { FeatureListComponent } from './ui/feature-list/feature-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, FeatureListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
