5d378be658a640c0ee944c34b0c1da15a72f5663
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-06-25 12:15:29 +0100

    Create `feature-list` component

---
+++ src/app/domains/home/ui/feature-list/feature-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'feature-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './feature-list.component.html',
+  styleUrl: './feature-list.component.scss',
+})
+export class FeatureListComponent {}
