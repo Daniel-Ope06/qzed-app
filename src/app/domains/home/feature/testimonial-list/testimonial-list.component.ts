07fc0d46a44b33e580dd173d9842518ddc139859
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-06-26 15:14:05 +0100

    Set up `testimonial-list` component

--- src/app/domains/home/feature/testimonial-list/testimonial-list.component.ts
+++ src/app/domains/home/feature/testimonial-list/testimonial-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'testimonial-list',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './testimonial-list.component.html',
   styleUrl: './testimonial-list.component.scss',
 })
-export class TestimonialListComponent {}
+export class TestimonialListComponent implements AfterViewInit {
+  @ViewChild('scroll') scroll!: ElementRef;
+  @ViewChild('innerScroll') innerScroll!: ElementRef;
+
+  testimonials = [1, 2, 3, 4, 5, 6];
+
+  ngAfterViewInit() {
+    // If a user hasn't opted in for reduced motion, then we add the animation
+    if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+      this.addAnimation();
+    }
+  }
+
+  addAnimation() {
+    this.scroll.nativeElement.setAttribute('data-animated', true);
+    let content = Array.from(this.innerScroll.nativeElement.children);
+    content.forEach((item) => {
+      let duplicatedItem = (item as HTMLElement).cloneNode(true) as HTMLElement;
+      duplicatedItem.setAttribute('aria-hidden', 'true');
+      this.innerScroll.nativeElement.appendChild(duplicatedItem);
+    });
+  }
+}
