1e1dcff81eca5a167e36ab8cb482525cbf790769
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-06-26 11:52:10 +0100

    Create `testimonial-list` component

---
+++ src/app/domains/home/feature/testimonial-list/testimonial-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'testimonial-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './testimonial-list.component.html',
+  styleUrl: './testimonial-list.component.scss',
+})
+export class TestimonialListComponent {}
