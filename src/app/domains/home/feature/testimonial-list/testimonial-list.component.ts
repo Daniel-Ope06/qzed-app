d4f512aec63fed5fe15f74df084abf4fe546e7af
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2024-06-28 12:50:49 +0100

    Link `testimonial-list` to the database

--- src/app/domains/home/feature/testimonial-list/testimonial-list.component.ts
+++ src/app/domains/home/feature/testimonial-list/testimonial-list.component.ts
@@ -1,32 +1,41 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  OnInit,
+  inject,
+} from '@angular/core';
+import { TestimonialService } from '../../data-access/testimonial.service';
+import { Testimonial } from '../../data-access/testimonial.model';
+import { LoadingSpinnerComponent } from '../../../shared/ui/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'testimonial-list',
   standalone: true,
-  imports: [],
+  imports: [LoadingSpinnerComponent],
   templateUrl: './testimonial-list.component.html',
   styleUrl: './testimonial-list.component.scss',
 })
-export class TestimonialListComponent implements AfterViewInit {
+export class TestimonialListComponent implements AfterViewInit, OnInit {
   @ViewChild('scroll') scroll!: ElementRef;
   @ViewChild('innerScroll') innerScroll!: ElementRef;
 
-  testimonials = [1, 2, 3, 4, 5, 6];
+  testimonialService: TestimonialService = inject(TestimonialService);
+  testimonials: Testimonial[] = [];
 
-  ngAfterViewInit() {
-    // If a user hasn't opted in for reduced motion, then we add the animation
-    if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-      this.addAnimation();
-    }
+  ngOnInit(): void {
+    this.testimonialService.getTestimonials().then((testimonials) => {
+      this.testimonials = testimonials;
+      // Add testimonials twice for smooth animation
+      this.testimonials.push(...this.testimonials);
+    });
   }
 
-  addAnimation() {
-    this.scroll.nativeElement.setAttribute('data-animated', true);
-    let content = Array.from(this.innerScroll.nativeElement.children);
-    content.forEach((item) => {
-      let duplicatedItem = (item as HTMLElement).cloneNode(true) as HTMLElement;
-      duplicatedItem.setAttribute('aria-hidden', 'true');
-      this.innerScroll.nativeElement.appendChild(duplicatedItem);
-    });
+  ngAfterViewInit(): void {
+    // If a user hasn't opted in for reduced motion, then we add the animation
+    if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+      this.scroll.nativeElement.setAttribute('data-animated', true);
+    }
   }
 }
